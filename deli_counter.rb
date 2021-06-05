aa0075ed89d3b0f77c6cead26ccee139a024ffe8
Brinton <brintonfoyreed@gmail.com>
2021-06-05 08:37:56 -0700

    commit tree 626438f2d71390466b264255e757041340c9de46

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+require "pry"
+
+katz_deli = ["bill", "jud", "snpt", "berg"] 
+name = "Teeder"
+
+def line (names)
+    if names.length > 0
+        first_line = "The line is currently:"
+        names.each.with_index(1) do |name, idx|
+            p first_line << "#{idx}. #{name}"
+        end
+    else
+        first_line = "The line is currently empty."    
+    end
+end
+
+def take_a_number(katz_deli, name)
+    p "#{name} " + katz_deli.length.to_s
+end
+
+
+line(katz_deli)
+take_a_number(katz_deli, name)
